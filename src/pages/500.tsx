cdbdc5aa2c89aa26f76eea7d22b6b516c9b38566
kaisa <kaisa@a-na.me>
2024-11-05 08:08:58 +0900

    ..

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,29 +1,20 @@
+import React from "react";
+
 export default function Custom500() {
   return (
-    <div>
-      <div id="wrapper">
-        <div id="container">
-          <section className="error-page">
-            <h2>요청하신 페이지를 찾을 수 없습니다.</h2>
-            <p>
-              입력하신 주소가 올바르지 않거나
-              <br/>
-              사용이 일시 중단되어 요청하신 페이지를 찾을 수 없습니다.
-              <br/>
-              <br/>
-              서비스 이용에 불편을 드려 죄송합니다.
-            </p>
-            <div className="mgt50px">
-              <button
-                className="btns ver-large blueBtn"
-                onClick={() => (location.href = '/')}
-              >
-                확인
-              </button>
-            </div>
-          </section>
-        </div>
-      </div>
+    <div id="error">
+      <div className="logo"><img src={process.env.NEXT_PUBLIC_IMG_HOST + '/common/logo.png'} width={140} alt=''/></div>
+      <h2>알수 없는 에러가 발생하였습니다.</h2>
+      <p>
+        서비스 이용에 불편을 드려 죄송합니다.
+      </p>
+      <button
+        className="big"
+        type="button"
+        onClick={() => (location.href = '/')}
+      >
+        홈으로
+      </button>
     </div>
   );
 }
